40e66adb08dcdc6f9440360e9d0087e097705200
konafx <afxlieonon@gmail.com>
2018-01-18 06:19:52 -0800

    hge

--- .zsh/conf/zplug.zsh
+++ .zsh/conf/zplug.zsh
@@ -1,26 +1,26 @@
-# zplug.zsh
-
+## zplug.zsh
+#
 #source ~/.zplug/init.zsh
 #
 ## Define plugins ##zplug "zsh-users/zsh-history-substring-search"
 ##
-##zplug "mollifier/cd-gitroot"
+#zplug "mollifier/cd-gitroot"
 ## リビジョンロック機能を持つ
 #
 #zplug "b4b4r07/enhancd"
-##zplug "mollifier/anyframe"
+#zplug "mollifier/anyframe"
 #
-### Gist ファイルもインストールできる
-###zplug "b4b4r07/79ee61f7c140c63d2786", \
-###	from:gist, \
-###	as:command, \
-###	use:get_last_pane_path.sh
-###
-#### bitbucket も
-###zplug "b4b4r07/hello_bitbucket", \
-###	from:bitbucket, \
-###	as:command, \
-###	use:"*.sh"
+## Gist ファイルもインストールできる
+#zplug "b4b4r07/79ee61f7c140c63d2786", \
+#	from:gist, \
+#	as:command, \
+#	use:get_last_pane_path.sh
+#
+## bitbucket も
+#zplug "b4b4r07/hello_bitbucket", \
+#	from:bitbucket, \
+#	as:command, \
+#	use:"*.sh"
 #
 ## 未インストール項目をインストールする
 #if ! zplug check --verbose; then
