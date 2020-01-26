d085a426222c0357a7020767152fa9e53f7cdc3c
konafx <afxlieonon@gmail.com>
2020-01-26 22:40:27 +0900

    Search vim-plugin and edit plugconf (Volt) C-v で起動できる

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,7 +29,6 @@ alias gst='git status'
 
 # For github local, remote
 alias gh='hub browse (ghq list | fzf | cut -d "/" -f 2,3)'
-alias v="vim $VOLTPATH/plugconf/(volt list | sed -r 's/^[\ \t]+//g; 1,2d' | fzf --ansi).vim"
 
 alias ssh_vagrant='sudo ssh -l vagrant -p 2222 -i /mnt/z/Vagrant/.vagrant/machines/default/virtualbox/private_key 127.0.0.1'
 
