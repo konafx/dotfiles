12ef5d5cfb678a689e8c92577dd220206c1c5fcd
konafx <afxlieonon@gmail.com>
2020-02-17 23:50:12 +0900

    Goodbye alias hub browse (install github-cli) and more vim pluginsのアップデートがあります

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,7 +15,7 @@ alias g='git'
 alias gst='git status'
 
 # For github local, remote
-alias gh='hub browse (ghq list | fzf | cut -d "/" -f 2,3)'
+#alias gh='hub browse (ghq list | fzf | cut -d "/" -f 2,3)'
 
 alias ssh_vagrant='sudo ssh -l vagrant -p 2222 -i /mnt/z/Vagrant/.vagrant/machines/default/virtualbox/private_key 127.0.0.1'
 
