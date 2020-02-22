49c73693e422d2ef8e6a36492dfa61c50c1783d8
konafx <afxlieonon@gmail.com>
2020-02-22 10:17:22 +0900

    Change eff.fish: edit fishfile

--- .config/fish/functions/eff.fish
+++ .config/fish/functions/eff.fish
@@ -1,18 +1,10 @@
-function eff -d 'edit fish functions'
-    set -l fish_func_path $HOME/.config/fish/functions
-
-    set -l func_name
-    if test -n "$argv[1]"
-        set func_name $argv[1]
-        # Add suffix ".fish"
-        string match -qr '\.fish$' $func_name; or set func_name $func_name.fish
-    else
-        set func_name (ls $fish_func_path --color | fzf --ansi --tac)
-    end
+function eff -d 'edit fish file' -a maxdepth
+    [ -z "$maxdepth" ]; and set maxdepth 2
+    set fishfile (find $__fish_config_dir -type f,l -maxdepth $maxdepth | sed -e "s@$__fish_config_dir@@" | fzf)
 
     set -l editor vim
     [ -n "$EDITOR" ]; and set editor $EDITOR
 
-    [ -n "$func_name" ]; and eval "$editor" $fish_func_path/$func_name
+    [ -n "$fishfile" ]; and eval "$editor" $__fish_config_dir$fishfile
     commandline -f repaint
 end
