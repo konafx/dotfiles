bbfe3788a45cd4a69decefc79858fd0b0d3ba9fd
konafx <afxlieonon@gmail.com>
2020-02-24 00:40:46 +0900

    Add fish-function: ev (edit vimrc)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,5 @@
 umask 022
 
-# Edit rc file
-alias ev='vim $HOME/.vimrc'
-
 alias reload='exec fish'
 
 # alias
@@ -12,11 +9,6 @@ alias mkdir='mkdir -p'
 alias v='vim'
 alias g='git'
 alias gst='git status'
-
-# For github local, remote
-#alias gh='hub browse (ghq list | fzf | cut -d "/" -f 2,3)'
-
-alias ssh_vagrant='sudo ssh -l vagrant -p 2222 -i /mnt/z/Vagrant/.vagrant/machines/default/virtualbox/private_key 127.0.0.1'
 
 # bobthefish
 set -g theme_display_date yes
