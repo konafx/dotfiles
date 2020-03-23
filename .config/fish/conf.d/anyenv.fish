6ffcf162a5dc66b0f71768d7f61ea2fac2c13248
konafx <22848261+konafx@users.noreply.github.com>
2020-03-23 13:07:02 +0900

    Fast boot

--- .config/fish/conf.d/anyenv.fish
+++ .config/fish/conf.d/anyenv.fish
@@ -1,4 +1,6 @@
-status --is-interactive; and source (anyenv init -|psub)
+status --is-interactive; and source (anyenv init - --no-rehash |psub)
+# command -q (anyenv root)/plugins/anyenv-lazyload/bin/anyenv-lazyload
+# and status -- is-interactive; and eval "(anyenv lazyload | psub)"
 
 # php-build
 set -x PHP_BUILD_EXTRA_MAKE_ARGUMENTS "-j4"
