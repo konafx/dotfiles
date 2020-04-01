05e7443d88d68e1b9ce59230a4bdec64d6dc3eb4
konafx <22848261+konafx@users.noreply.github.com>
2020-04-01 15:23:51 +0900

    edit C-v: search from all plugin, and volt update

--- .config/fish/functions/__volt_plugin_search.fish
+++ .config/fish/functions/__volt_plugin_search.fish
@@ -1,6 +1,6 @@
 function __volt_plugin_search -d 'Search config file of vim plugin managed volt'
     set -l selected_plugin
-    volt list | sed -r 's/^[\ \t]+//g; 1,2d' | fzf --ansi | read selected_plugin
+    volt list -f "{{ range .Repos }}{{ println .Path }}{{ end }}" | fzf --ansi | read selected_plugin
     [ -n "$selected_plugin" ]; and vim $VOLTPATH/plugconf/$selected_plugin.vim
     commandline -f repaint
 end
