979992880fe8d1c8a6ce025168222885a69f55fa
konafx <afxlieonon@gmail.com>
2020-04-01 15:34:30 +0900

    vim sonictemplate 参考 https://qiita.com/gorilla0513/items/d1b826137aa35890f4df

---
+++ .vim/sonictemplate/vue/base-ts_scss.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class {{_name_}} extends Vue { {{_cursor_}}}
+</script>
+
+<style scoped lang="scss">
+
+</style>
