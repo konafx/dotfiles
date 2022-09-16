8296d1f7d6df163b15e57424e89ce3c0f71aa785
konafx <afxlieonon@gmail.com>
2022-09-16 08:40:03 +0000

    add plugins from vimrc

---
+++ nvim/after/plugin/clever-f.rc.lua
@@ -0,0 +1,2 @@
+vim.keymap.set({ 'n', 'x', 'o' }, 'f', '<Plug>(clever-f-f)', { noremap = false })
+vim.keymap.set({ 'n', 'x', 'o' }, 'F', '<Plug>(clever-f-F)', { noremap = false })
---
+++ nvim/after/plugin/vim-operator-surround.rc.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('', 'sa', '<Plug>(operator-surround-append)', { silent = true })
+vim.keymap.set('', 'sd', '<Plug>(operator-surround-delete)', { silent = true })
+vim.keymap.set('', 'sr', '<Plug>(operator-surround-replace)', { silent = true })
