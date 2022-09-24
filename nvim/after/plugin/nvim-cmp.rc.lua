d948c7a1ab543dc879ac19de5a89e122b0fa6a3d
konafx <22848261+konafx@users.noreply.github.com>
2022-09-24 08:08:15 +0000

    fix nvim-cmp luasnip setting, add caw.vim

--- nvim/after/plugin/nvim-cmp.rc.lua
+++ nvim/after/plugin/nvim-cmp.rc.lua
@@ -3,15 +3,21 @@ if not ok then return end
 local ok, lspkind = pcall(require, 'lspkind')
 if not ok then return end
 
+local ok, luasnip = pcall(require, 'luasnip')
+if not ok then print('luasnip is not installed') end
+
+local has_words_before = function()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  return (vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or ''):sub(cursor[2], cursor[2]):match('%s')
+end
+
 cmp.setup {
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-l>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
@@ -19,7 +25,7 @@ cmp.setup {
       select = true
     }),
     -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-n>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -30,7 +36,7 @@ cmp.setup {
         fallback()
       end
     end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["<C-p>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
