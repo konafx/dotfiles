24e2a23fd2c30b6cf8c9790e8afe0f65b7f80e0e
konafx <afxlieonon@gmail.com>
2022-10-06 18:22:38 +0900

    [WIP] Add autocmd for shutup folding after EVERY writing
    - Bug: plugings.luaをwriteするときだけ全部閉じる
    - ref:
      - https://stackoverflow.com/questions/37552913/vim-how-to-keep-folds-on-save
      - https://vim-jp.slack.com/archives/CJMV3MSLR/p1657680973497009
        - telescopeのwindowなどでバグるのを回避する

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,39 +1,39 @@
 local create_augroup = vim.api.nvim_create_augroup
 local create_autocmd = vim.api.nvim_create_autocmd
 
-create_autocmd('filetype', {
+create_autocmd('filetype', { -- {{{
   group = create_augroup('DisableInsertNextlineComment', {}),
   pattern = '*',
   command = 'setlocal formatoptions-=ro'
-})
+}) -- }}}
 
--- Turn off paste mode when leaving insert
+-- Turn off paste mode when leaving insert{{{
 create_autocmd('InsertLeave', {
   pattern = '*',
   command = 'set nopaste'
-})
+}) -- }}}
 
--- QuickFixGrep
+-- QuickFixGrep{{{
 create_autocmd('QuickFixCmdPost', {
   group = create_augroup('QuickFixGrep', {}),
-  pattern = {'make', '*grep*'},
+  pattern = { 'make', '*grep*' },
   command = 'cwindow',
-})
+}) -- }}}
 
--- Automaticaly Packer Reload
+-- Automaticaly Packer Reload{{{
 -- https://github.com/wbthomason/packer.nvim/blob/6afb67460283f0e990d35d229fd38fdc04063e0a/README.md?plain=1#L202-L222
 create_autocmd('BufWritePost', {
   group = create_augroup('PackerUserConfig', {}),
   pattern = 'plugins.lua',
   command = 'source <afile> | PackerCompile',
-})
+}) -- }}}
 
--- 背景透過
+-- 背景透過{{{
 if vim.env.TRANSPARENT_TERM == '1' then
   create_autocmd('Colorscheme', {
     group = vim.api.nvim_create_augroup('TransparentBackground', {}),
     pattern = '*',
-    callback = function ()
+    callback = function()
       vim.api.nvim_set_hl(0, 'Normal', { ctermbg = 'NONE' })
       vim.api.nvim_set_hl(0, 'NonText', { ctermbg = 'NONE' })
       vim.api.nvim_set_hl(0, 'LineNr', { ctermbg = 'NONE' })
@@ -41,4 +41,17 @@ if vim.env.TRANSPARENT_TERM == '1' then
       vim.api.nvim_set_hl(0, 'EndOfBuffer', { ctermbg = 'NONE' })
     end,
   })
-end
+end -- }}}
+
+-- disable autofold at writing{{{
+local shut_up_auto_fold_at_writing = create_augroup('ShutUpAutoFoldAtWriting', {})
+create_autocmd('BufWinLeave', {
+  group = shut_up_auto_fold_at_writing,
+  pattern = '*',
+  command = [[if expand('%') != '' && &buftype != 'nofile' | mkview | endif]],
+})
+create_autocmd('BufWinEnter', {
+  group = shut_up_auto_fold_at_writing,
+  pattern = '*',
+  command = [[if expand('%') != '' && &buftype != 'nofile' | silent! loadview | endif]],
+}) -- }}}
