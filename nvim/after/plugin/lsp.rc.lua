8a14cd06d23245d7ad7de1bbf7c653af2b072917
konafx <22848261+konafx@users.noreply.github.com>
2023-01-08 20:03:55 +0900

    update nvim rc

--- nvim/after/plugin/lsp.rc.lua
+++ nvim/after/plugin/lsp.rc.lua
@@ -96,7 +96,7 @@ local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not ok then print('cmp-nvim-lsp is not installed') return end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 --- }}}
 
 --- mason lspconfig {{{
