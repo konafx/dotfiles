1be91b4fbeff8373fc72d283be6ff73da4db96cc
konafx <afxlieonon@gmail.com>
2023-09-08 18:34:00 +0900

    add: welcome lspsaga

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -92,15 +92,17 @@ lazy.setup({
     event = 'BufRead',
     config = function ()
       require('lspsaga').setup({
+        lightbulb = {
+          enable = false
+        },
         symbol_in_winbar = {
           enable = false,
-        }
+        },
       })
     end,
     dependencies = {
       'nvim-tree/nvim-web-devicons'
     },
-enabled = false,
   },
 
 	-- complete
