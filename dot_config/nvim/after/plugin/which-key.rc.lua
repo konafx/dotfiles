8c8c84c10af2fe4644561ce82eaacf9f41d4333c
konafx <22848261+konafx@users.noreply.github.com>
2025-04-08 21:00:48 +0900

    fix: which-key update対応

--- dot_config/nvim/after/plugin/which-key.rc.lua
+++ dot_config/nvim/after/plugin/which-key.rc.lua
@@ -4,17 +4,15 @@ if not ok then
 end
 
 -- [lsp]を押しても表示されない. :WhichKey [lsp]には登録されている
-wk.register({
-	['['] = { '<Cmd>Lspsaga diagnostic_jump_prev<CR>' },
-	[']'] = { '<Cmd>Lspsaga diagnostic_jump_next<CR>' },
-	e = { '<Cmd>Lspsaga show_cursor_diagnostics<CR>' },
-	D = { 'Declaration' },
-	['<C-d>'] = { 'vim.lsp.buf.definition' },
-	d = { '<Cmd>Lspsaga peek_definition<CR>' },
-	i = { 'Implementation' },
-	f = { '<Cmd>Lspsaga lsp_finder<CR>' },
-	F = { 'Format' }
-}, {
-	mode = 'n',
-	prefix = '[lsp]',
+wk.add({
+	mode = {"n"},
+	{ "[lsp]<C-d>", desc = "vim.lsp.buf.definition" },
+	{ "[lsp][", desc = "<Cmd>Lspsaga diagnostic_jump_prev<CR>" },
+	{ "[lsp]]", desc = "<Cmd>Lspsaga diagnostic_jump_next<CR>" },
+	{ "[lsp]e", desc = "<Cmd>Lspsaga show_cursor_diagnostics<CR>" },
+	{ "[lsp]d", desc = "<Cmd>Lspsaga peek_definition<CR>" },
+	{ "[lsp]f", desc = "<Cmd>Lspsaga lsp_finder<CR>" },
+	{ "[lsp]D", desc = "Declaration" },
+	{ "[lsp]F", desc = "Format" },
+	{ "[lsp]i", desc = "Implementation" },
 })
