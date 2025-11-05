17a78bd79ef3ed742f4a19f14899de5c315d7ad3
konafx <22848261+konafx@users.noreply.github.com>
2025-11-05 16:29:12 +0900

    docs: obsidian.nvim local setting usage

--- dot_config/nvim/after/plugin/empty_obsidian.rc.lua
+++ dot_config/nvim/after/plugin/empty_obsidian.rc.lua
@@ -1,4 +1,15 @@
 local local_config = vim.fn.stdpath('config') .. "/local/obsidian.lua"
 if vim.fn.filereadable(local_config) == 1 then
 	dofile(local_config)
+	-- Sample:
+	-- ```
+	-- require('obsidian').setup({
+	-- 	workspaces = {
+	-- 		{
+	-- 			name = "hoge",
+	-- 			path = "/path/to/hoge",
+	-- 		}
+	-- 	}
+	-- })
+	-- ```
 end
